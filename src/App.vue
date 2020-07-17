d0d3c86986e0b224117559869f3469872d1e61f5
fx67ll <2717745014@qq.com>
2020-07-17 15:40:18 +0800

    ez13项目开始

---
+++ src/App.vue
@@ -0,0 +1,29 @@
+<template>
+  <div id="app">
+	  <Index></Index>
+  </div>
+</template>
+
+<script>
+import Index from './components/index.vue'
+
+export default {
+  name: 'app',
+  components: {
+    Index
+  }
+}
+</script>
+
+<style>
+html,body{
+	width: 100%;
+	height: 100%;
+	margin: 0;
+	padding: 0;
+}
+#app {
+	width: 100%;
+	height: 100%;
+}
+</style>
